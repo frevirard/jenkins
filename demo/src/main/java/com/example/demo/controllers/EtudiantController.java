ffdbe4f8a2ff0992a4ae248236152085507b0fb3
frevirard <frevirard@gmail.com>
2024-06-13 11:57:26 +0200

    'Correction controller

--- demo/src/main/java/com/example/demo/controllers/EtudiantController.java
+++ demo/src/main/java/com/example/demo/controllers/EtudiantController.java
@@ -1,17 +1,18 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("etudiant")
+// @RequestMapping("/etudiant")
 public class EtudiantController {
 
-    @PostMapping("enregistrer")
-    public String saveEtudiant(@RequestBody String param) {
-        String resulat = param.equalsIgnoreCase("edem") ? "cool" : "Vous êtes pas le bienvenu";
-        return resulat;
+    @GetMapping("/enregistrer")
+    public String saveEtudiant() {
+        return "ok, l'appli est déployée sur tomcat";
     }
 }
